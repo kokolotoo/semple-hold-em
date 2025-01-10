6717cb11a6f6081783906e358f53b17427795450
kokoloto <chapi.kaica@gmail.com>
2025-01-10 18:37:04 +0200

    add welcome message

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Welcome to my sample app</h1>
+      <br />
+      <p>In working progress</p>
     </div>
   );
 }
