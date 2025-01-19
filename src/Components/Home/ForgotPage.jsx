cedc01e06a20bc73a85be55a68d9733648b85b9a
kokoloto <chapi.kaica@gmail.com>
2025-01-19 12:14:00 +0200

    add forgot-pass functionality

--- src/Components/Home/ForgotPage.jsx
+++ src/Components/Home/ForgotPage.jsx
@@ -1,16 +1,49 @@
 import React, { useState, useContext } from 'react';
 import './home.css'
 import DataContext from '../../Context/DataContext';
-import { useNavigate } from 'react-router';
+import { Link } from 'react-router';
 
 const ForgotPage = () => {
+ 
+  const { profiles } = useContext(DataContext)
+  const [searchEmail, setSearchEmail] = useState('')
+  const [searchResult, setSearchResult] = useState(null)
 
-    const {profiles} = useContext(DataContext)
-
+  const checkForPassword = (e) => {
+    e.preventDefault()
+    const findMatch = profiles.filter(profile => profile.email === searchEmail)
+    if (findMatch.length > 0) {
+      setSearchResult(`Username: ${findMatch[0].username} / pass: ${findMatch[0].password}`)
+    } else {
+      setSearchResult(`Not profile with : ${searchEmail}`)
+    }
+    setSearchEmail('')
+  }
 
   return (
     <main className='forgot-container'>
-          <h2> In working progress</h2>
+
+      {searchResult && <h3>{searchResult}</h3>}
+
+      <form onSubmit={checkForPassword}>
+
+        <label htmlFor="email">Enter email:</label>
+        <input type="email"
+          placeholder='email'
+          value={searchEmail}
+          onChange={(e) => setSearchEmail(e.target.value)}
+          required
+        />
+
+        <button>Submit</button>
+      </form>
+
+      <section className='navigation-container'>
+        <Link to='/login'>Login</Link>
+        <Link to='/register'>Registration</Link>
+        <Link to='/game'>Demo Game</Link>
+      </section>
+
     </main>
   );
 }
